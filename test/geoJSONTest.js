9c881b3b118ef2863f10175371c3ffeb27d29b96
Gabrielle Young <gmyoung321@gmail.com>
2016-02-08 17:30:14 +1300

    fix failing geoJSON creation test, kudos to pete

--- test/geoJSONTest.js
+++ test/geoJSONTest.js
@@ -10,6 +10,7 @@ describe('createGeoJSON()', function () {
  it('should return geoJSON, given JSON in the format {title:x, number_of_links:x, image_link:x }', function(){
   // this is the exact JSON, but it won't pass, not sure how to compare JSON!
   var geoJSONofTestData = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"marker-color":"#78f440","marker-size":"small","marker-symbol":"heart","title":"xxx","description":"yyy"},"geometry":{"type":"Point","coordinates":[167.92404690648695,-46.93866307676541]}},{"type":"Feature","properties":{"marker-color":"#78f440","marker-size":"small","marker-symbol":"heart","title":"xxx","description":"yyy"},"geometry":{"type":"Point","coordinates":[168.34441319643508,-46.59389464750951]}},{"type":"Feature","properties":{"marker-color":"#78f440","marker-size":"small","marker-symbol":"heart","title":"xxx","description":"yyy"},"geometry":{"type":"Point","coordinates":[168.76213793130225,-46.249114578957006]}}]}
-  expect(geoJSON.createGeoJSON(testData.testData)).to.equal(geoJSONofTestData);
+  var createdGeoJSON = JSON.stringify(geoJSON.createGeoJSON(testData.testData))
+  expect(createdGeoJSON).to.equal(JSON.stringify(geoJSONofTestData));
 });
 });
